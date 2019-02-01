f11fae824738d26bc5f39d7f42f8c13ca229cfa4
full stack node react mysql <kari.eronenoy@gmail.com>
2019-02-01 16:39:41 +0200

    1.st prod

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+//production keys here
+
+module.exports = {
+
+googleClientID: process.env.GOOGLE_CLIENT_ID,
+//'669034769809-6tdv4pphgmtj18ebrlb9dhufei7raa3m.apps.googleusercontent.com',
+googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//'Jp61CgWniq2GQvbuP93lMIbq',
+
+mongoURI: process.env.MONGO_URI,
+//'mongodb://emailyprod:emailyprod007@ds261644.mlab.com:61644/emailykari-prod',
+cookieKey: process.env.COOKIE_KEY
+};
