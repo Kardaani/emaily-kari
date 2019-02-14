16790513e92eb976fdaa1af3d795d9245c18bc2d
full stack node react mysql <kari.eronenoy@gmail.com>
2019-02-14 19:24:16 +0200

    after sendgrid

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,10 @@ import App from './components/App';
 
 import reducers from './reducers';
 
+
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 //const store = createStore(() => [], {}, applyMiddleware(reduxThunk));
