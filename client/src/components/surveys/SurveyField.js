15c1da890d10960fe725583ae56d69653cba6902
full stack node react mysql <kari.eronenoy@gmail.com>
2019-02-21 15:42:31 +0200

    Drying up fields

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,16 @@
+//SurveyField contains logic to render single label
+//and text input
+
+import React from 'react';
+
+export default ({ input, label }) => {
+
+return (
+  <div>
+  <label>{label}</label>
+    <input {...input} />
+  </div>
+
+   );
+
+};
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,14 +1,63 @@
 //SurveyForm shows a form a user to add input
 
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+{label: 'Survey Title', name: 'title'},
+{label: 'Subject Line', name: 'subject'},
+{label: 'Email Body', name: 'body'},
+{label: 'Recipient Lists', name: 'emails'}
+
+];
 
 class SurveyForm extends Component {
+
+
+//helper function renderFields
+
+renderFields(){
+  return (
+    <div>
+      <Field
+      label="Survey title"
+       type="text"
+        name="title"
+         component={SurveyField}
+      />
+      <Field
+      label="Subject Line"
+       type="text"
+        name="subject"
+         component={SurveyField}
+      />
+      <Field
+      label="Email Body"
+       type="text"
+        name="body"
+         component={SurveyField}
+      />
+      <Field
+      label="Recipient List"
+       type="text"
+        name="emails"
+         component={SurveyField}
+      />
+    </div>
+  );
+}
+
+//render function tulostukseen
 
   render() {
     return (
       <div>
-        SurveyForm!
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        {this.renderFields()}
+
+        <button type="submit">Submit</button>
+        </form>
       </div>
     );
 
