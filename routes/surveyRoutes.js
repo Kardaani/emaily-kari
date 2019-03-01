1a0188888b6f18eb09bd45168ab05fb3794cec0a
full stack node react mysql <kari.eronenoy@gmail.com>
2019-03-01 13:16:37 +0200

    tunneling setup crash ?

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,8 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
+
+
 const mongoose = require('mongoose');
 
 const requireLogin = require('../middlewares/requireLogin');
@@ -12,6 +17,19 @@ module.exports = app => {
 
   app.get('/api/surveys/thanks', (req,res) => {
     res.send('Thanks for voting!');
+  });
+
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path('/api/surveys/:surveyId/:choice');
+          const match = p.test(pathname);
+          if (match) {
+            return { email, surveyId: match.surveyId, choice: match.choice };
+          }
+    });
+console.log(events);
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) =>  {
